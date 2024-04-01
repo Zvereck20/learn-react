54f4bd8ffe5c1f66d2345ecb466ce2407297552b
Zvereck20 <kerusov20@gmail.com>
2024-04-01 22:17:52 +1000

    Without JSX

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import { createElement } from 'react';
 const date = new Date();
 
 export const App = () => {
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement('header', { className: 'App-header' }, [
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement('p', null, 'Edit <code>src/App.js</code> and save to reload.'),
-			createElement('a', { className: 'App-link', href: 'https://reactjs.org', target: '_blank', rel: 'noopener noreferrer' }, 'Learn React'),
-			createElement('div', { style: { marginTop: '20px' } }, String(date.getFullYear())),
-		]),
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+				<div>{String(date.getFullYear())}</div>
+			</header>
+		</div>
 	);
 };

9a05410417947057858442f863abb02ab2a9356c
Zvereck20 <kerusov20@gmail.com>
2024-04-01 22:15:14 +1000

    With JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const date = new Date();
 
-export default App;
+export const App = () => {
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement('header', { className: 'App-header' }, [
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement('p', null, 'Edit <code>src/App.js</code> and save to reload.'),
+			createElement('a', { className: 'App-link', href: 'https://reactjs.org', target: '_blank', rel: 'noopener noreferrer' }, 'Learn React'),
+			createElement('div', { style: { marginTop: '20px' } }, String(date.getFullYear())),
+		]),
+	);
+};

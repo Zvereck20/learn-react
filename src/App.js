1f75ce858eb1c02bfa8c5e1853bd5c26d8e08b42
Zvereck20 <kerusov20@gmail.com>
2024-04-19 14:40:16 +1000

    Frontend - 2 - 3

--- src/App.js
+++ src/App.js
@@ -1,71 +1,114 @@
 import styles from './App.module.css';
 import { useState } from 'react';
 
+const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let result = '';
+
 export const App = () => {
-	const [value, setValue] = useState(false);
-	const [list, setList] = useState([]);
-	const [error, setError] = useState('');
-	const [isValueValid, setIsValueValid] = useState(false);
+	const [value, setValue] = useState('...');
+	const [operand1, setOperand1] = useState('');
+	const [operand2, setOperand2] = useState('');
+	const [operator, setOperator] = useState('');
 
-	const onInputButtonClick = () => {
-		let promptValue = prompt('Введите значение');
+	const numClick = (el) => {
+		operator.length
+			? operand2.length
+				? (setValue(operand1 + operator + operand2 + el.target.value),
+					setOperand2((op) => op + el.target.value))
+				: (setValue(operand1 + operator + el.target.value),
+					setOperand2(el.target.value))
+			: operand1.length
+				? (setValue(operand1 + el.target.value),
+					setOperand1((op) => op + el.target.value))
+				: (setOperand1(el.target.value), setValue(el.target.value));
 
-		if (promptValue.length < 3) {
-			setError('Введенное значение должно содержать минимум 3 символа');
-			setIsValueValid(false);
-		} else {
-			setValue(promptValue);
-			setError('');
-			setIsValueValid(true);
-		}
+		result.length ? (result = '') : null;
 	};
 
-	const onAddButtonClick = () => {
-		if (value.length > 2) {
-			let id = Date.now();
-			let updatedList = [...list, { id, value }];
-			setList(updatedList);
-			setError('');
-			setValue('');
-			setIsValueValid(false);
-		}
+	const operatorClick = (el) => {
+		operand1.length &&
+			(setValue(operand1 + el.target.value), setOperator(el.target.value));
+
+		result.length ? (result = '') : null;
+	};
+
+	const clearClick = () => {
+		setValue('...');
+		setOperand1('');
+		setOperand2('');
+		setOperator('');
+
+		result.length ? (result = '') : null;
+	};
+
+	const getResult = () => {
+		result = String(eval(value));
+		setValue(result);
+		setOperand1(result);
+		setOperand2('');
+		setOperator('');
 	};
 
 	return (
-		<div className={styles.app}>
-			<h1 className={styles.pageHeading}>Ввод значения</h1>
-			<p className={styles.noMarginText}>
-				Текущее значение <code>value</code>: "<output className={styles.currentValue}>{value}</output>"
-			</p>
+		<div className={styles.container}>
+			<h2>Введите число</h2>
+			<div
+				className={
+					styles.valueContainer +
+					' ' +
+					(result.length ? `${styles.valueResult}` : '')
+				}
+			>
+				{value}
+			</div>
+			<div className={styles.buttonContainer}>
+				{NUMS.map((num) => {
+					return (
+						<button
+							key={num}
+							className={styles.numButton + ' ' + styles.button}
+							onClick={numClick}
+							value={num}
+							type="button"
+						>
+							{num}
+						</button>
+					);
+				})}
+			</div>
 
-			{error !== '' && <div className={styles.error}>{error}</div>}
-
-			<div className={styles.buttonsContainer}>
-				<button className={styles.button} onClick={onInputButtonClick}>
-					Ввести новое
+			<div className={styles.operationsContainer}>
+				<button
+					className={styles.operationButton + ' ' + styles.button}
+					disabled={operator.length && true}
+					onClick={operatorClick}
+					value="+"
+				>
+					+
 				</button>
-
-				<button className={styles.button} disabled={!isValueValid} onClick={onAddButtonClick}>
-					Добавить в список
+				<button
+					className={styles.operationButton + ' ' + styles.button}
+					disabled={operator.length && true}
+					onClick={operatorClick}
+					value="-"
+				>
+					-
+				</button>
+				<button
+					className={styles.operationButton + ' ' + styles.button}
+					onClick={clearClick}
+				>
+					C
 				</button>
 			</div>
-			<div className={styles.listContainer}>
-				<h2 className={styles.listHeading}>Список:</h2>
 
-				{list.length === 0 ? (
-					<p className={styles.noMarginText}>Нет добавленных элементов</p>
-				) : (
-					<ul className={styles.list}>
-						{list.map(({ id, value }) => {
-							return (
-								<li className={styles.listItem} key={id}>
-									{value}
-								</li>
-							);
-						})}
-					</ul>
-				)}
-			</div>
+			<button
+				className={styles.evenlyButton + ' ' + styles.button}
+				disabled={!operand2.length && true}
+				onClick={getResult}
+			>
+				=
+			</button>
 		</div>
 	);
 };

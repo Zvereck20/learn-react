7c2bf68d2958211acfe64a3ef10f64b7c5f481a7
Zvereck20 <kerusov20@gmail.com>
2024-06-17 19:53:48 +1000

    Frontend - 5 - 1

--- src/App.js
+++ src/App.js
@@ -1,74 +1,42 @@
-import { useEffect, useRef } from "react";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { fieldsSchema } from "./schemes/fields-scheme";
-import styles from "./styles.module.css";
-
-const sendFormData = (formData) => {
-  console.log(formData);
-};
+import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
-  const submitButtonRef = useRef(null);
+	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({
-    defaultValues: {
-      email: "",
-      password: "",
-      confirmPassword: "",
-    },
-    resolver: yupResolver(fieldsSchema),
-  });
+	useEffect(() => {
+		setIsLoading(true);
 
-  const emailError = errors.email?.message;
-  const passwordError = errors.password?.message;
-  const confirmPasswordError = errors.confirmPassword?.message;
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((loadedData) => loadedData.json())
+			.then((loadedProducts) => {
+				setProducts(loadedProducts);
+			})
+			.catch(() => console.log('some error'))
+			.finally(() => setIsLoading(false));
+	}, []);
 
-  useEffect(() => {
-    if (isValid) {
-      submitButtonRef.current.focus();
-    }
-  }, [isValid]);
-
-  return (
-    <div className={styles.app}>
-      <form onSubmit={handleSubmit(sendFormData)} className={styles.form}>
-        <fieldset>
-          <label htmlFor="email">Введите Email</label>
-          {emailError && <div className={styles.error}>{emailError}</div>}
-          <input name="email" type="text" {...register("email")} />
-        </fieldset>
-
-        <fieldset>
-          <label htmlFor="password">Введите пароль</label>
-          {passwordError && <div className={styles.error}>{passwordError}</div>}
-          <input name="password" type="password" {...register("password")} />
-        </fieldset>
-
-        <fieldset>
-          <label htmlFor="confirmPassword">Введите пароль повторно</label>
-          {confirmPasswordError && (
-            <div className={styles.error}>{confirmPasswordError}</div>
-          )}
-          <input
-            name="confirmPassword"
-            type="password"
-            {...register("confirmPassword")}
-          />
-        </fieldset>
-        <button
-          type="submit"
-          className={styles.button}
-          ref={submitButtonRef}
-          disabled={Object.keys(errors).length ? true : false}
-        >
-          Отправить
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<div className={styles.wrapper}>
+			{isLoading ? (
+				<div className="loader"></div>
+			) : (
+				<ul className={styles.list}>
+					{products.map(({ id, title, completed }) => (
+						<li
+							key={id}
+							className={
+								`${styles.item} ` +
+								(completed ? `${styles.completed}` : '')
+							}
+						>
+							<div className={styles.count}>{id}</div>
+							{title}
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 };
